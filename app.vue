3d07406e17ce8e8ba4001f7562fca37a336a2f5b
Oguz Han Asnaz <github@lostgeek.de>
2023-12-06 16:44:22 +0100

    Restyle pages

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
+  <div class="container">
+    <h1><NuxtLink class="headerlink" to="/">Advent of Code 2023</NuxtLink></h1>
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+.container{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+}
+
+.headerlink {
+  color: white;
+  text-decoration: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,36 @@
 <template>
     <div class="container">
-        <h1>Advent of Code 2023</h1>
         <div class="days">
             <NuxtLink to="/01">01</NuxtLink>
             <NuxtLink to="/02">02</NuxtLink>
             <NuxtLink to="/03">03</NuxtLink>
             <NuxtLink to="/04">04</NuxtLink>
+            <NuxtLink class="disabled" to="/">05</NuxtLink>
+            <NuxtLink class="disabled" to="/">06</NuxtLink>
+            <NuxtLink class="disabled" to="/">07</NuxtLink>
+            <NuxtLink class="disabled" to="/">08</NuxtLink>
+            <NuxtLink class="disabled" to="/">09</NuxtLink>
+            <NuxtLink class="disabled" to="/">11</NuxtLink>
+            <NuxtLink class="disabled" to="/">12</NuxtLink>
+            <NuxtLink class="disabled" to="/">13</NuxtLink>
+            <NuxtLink class="disabled" to="/">14</NuxtLink>
+            <NuxtLink class="disabled" to="/">15</NuxtLink>
+            <NuxtLink class="disabled" to="/">16</NuxtLink>
+            <NuxtLink class="disabled" to="/">17</NuxtLink>
+            <NuxtLink class="disabled" to="/">18</NuxtLink>
+            <NuxtLink class="disabled" to="/">19</NuxtLink>
+            <NuxtLink class="disabled" to="/">20</NuxtLink>
+            <NuxtLink class="disabled" to="/">21</NuxtLink>
+            <NuxtLink class="disabled" to="/">22</NuxtLink>
+            <NuxtLink class="disabled" to="/">23</NuxtLink>
+            <NuxtLink class="disabled" to="/">24</NuxtLink>
+            <NuxtLink class="disabled" to="/">25</NuxtLink>
         </div>
         <a href="https://github.com/lostgeek/aoc2023" target="_blank" class="github"><GitHub /> GitHub</a>
     </div>
 </template>
 
 <style lang="scss">
-
-.container{
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    align-items: center;
-}
 .days {
     display: flex;
     flex-direction: row;
@@ -29,6 +41,11 @@
     & a {
         width: 1rem;
         text-align: center;
+    }
+
+    & a.disabled {
+        color: grey;
+        text-decoration: none;
     }
 }
 
